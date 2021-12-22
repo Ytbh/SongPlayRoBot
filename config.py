ecadf1315415516e4b8e6be790b6390d84e7647d
Ytbh <89383662+Ytbh@users.noreply.github.com>
2021-12-23 01:11:03 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "19741948"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "1e7f122eacfa0832c65c0185eba77178" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "5089544837:AAEjqQ8PL_46ewPksZk2R3Ir4aiHagSi-L0" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
